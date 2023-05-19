746010da011172e78a9b89c8f7ef6d22c8683b50
Aarni Koskela <akx@iki.fi>
2023-05-19 10:41:19 +0300

    Optimize some forEach loops to for..of or just spreads; convert .map() without lvalue to for

--- js/app/src/Blocks.svelte
+++ js/app/src/Blocks.svelte
@@ -294,7 +294,8 @@
 					});
 
 					if (status.stage === "complete") {
-						dependencies.map(async (dep, i) => {
+						dependencies.forEach(async (dep, i) => {
+							// TODO: this does not await for the API calls to finish, is this a bug?
 							if (dep.trigger_after === fn_index) {
 								await trigger_api_call(i);
 							}
@@ -304,7 +305,8 @@
 					}
 
 					if (status.stage === "error") {
-						dependencies.map(async (dep, i) => {
+						dependencies.forEach(async (dep, i) => {
+							// TODO: this does not await for the API calls to finish, is this a bug?
 							if (
 								dep.trigger_after === fn_index &&
 								!dep.trigger_only_on_success
